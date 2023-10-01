55db2888158a892d42fc10f1c7563ca32fd678cd
“Dami-LEE00” <dxmxxii@gmail.com>
2023-10-02 03:46:31 +0900

    add func go to signin

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,14 @@
 /** @jsxImportSource @emotion/react */
 
 import { css } from "@emotion/react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    navigate('/signin');
+  }
   return (
     <div css={wrapper}>
       <h1 css={title}>Home</h1>
@@ -10,6 +16,7 @@ function Home() {
         <span>로그인 완료</span>
         <h3>💙 환영합니다 💙</h3>
       </div>
+      <button onClick={handleLogOut} css={btnLogOut}>로그아웃</button>
     </div>
   )
 }
@@ -21,11 +28,11 @@ const wrapper = css`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 1.25rem;
+  gap: 2.5rem;
 `;
 
 const title = css`
-  maring: 0;
+  margin: 0;
   padding: 0;
   font-size: 3rem;
   font-weight: 400;
@@ -44,9 +51,14 @@ const descWrapper = css`
     color: #999;
   }
   h3 {
-    maring: 0;
+    margin: 0;
     padding: 0;
     font-size: 1.5rem;
     font-weight: 400;
   }
+`;
+
+const btnLogOut = css`
+  width: 100%;
+  padding: .625rem 0;
 `;
