b967deed4fdb3e922a32d0381b95a178f8252b36
“Dami-LEE00” <dxmxxii@gmail.com>
2023-10-02 03:46:49 +0900

    add signup form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,39 @@
 /** @jsxImportSource @emotion/react */
 
-import { css } from "@emotion/react";
+import { wrapper, formWrapper, inputWrapper, btnWrapper } from "../../styles/FormStyle";
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
+  const navigate = useNavigate();
+
+  const handleSignUp = () => {
+    navigate('/signup');
+  }
+
   return (
     <div css={wrapper}>
-      SignIn
+      <h1>SignIn</h1>
+      <form css={formWrapper}>
+        <div css={inputWrapper}>
+          <label>Email</label>
+          <input
+            type="email"
+          />
+        </div>
+        <div css={inputWrapper}>
+          <label>Password</label>
+          <input
+            type="password"
+          />
+        </div>
+        <div css={btnWrapper}>
+          <button onClick={handleSignUp} type="button">회원가입</button>
+          <button type="submit">로그인</button>
+        </div>
+      </form>
     </div>
   )
 }
 
 export default SignIn;
 
-const wrapper = css`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`;
