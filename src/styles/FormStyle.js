b3fb7a0093e6f1c9b76ccefed7f9fb7655fe60d8
“Dami-LEE00” <dxmxxii@gmail.com>
2023-10-02 03:45:32 +0900

    common styles

---
+++ src/styles/FormStyle.js
@@ -0,0 +1,48 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
+
+export const wrapper = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2.5rem;
+`;
+
+export const formWrapper = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: .625rem;
+`;
+
+export const inputWrapper = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  label {
+    display: inline-block;
+    width: 10rem;
+  }
+  input {
+    width: 15rem;
+    padding: .3125rem .625rem;
+  }
+`;
+
+export const btnWrapper = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1.25rem;
+  width: 100%;
+  margin-top: 1.25rem;
+  button {
+    width: 100%;
+    padding: .625rem 0;
+  }
+`;
